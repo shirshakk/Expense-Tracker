10bbc2888518f0e3b22f9c84e35bd5b9a56f1186
shirshakk <kshirshak76@gmail.com>
2024-10-29 10:07:24 +0530

    Task add UI completed

--- lib/Widgets/new_expense.dart
+++ lib/Widgets/new_expense.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:expense_tracker/models/expense.dart';
+
+final formatter = DateFormat.yMd();
 
 class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
@@ -8,17 +12,111 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+  DateTime? _selecterDate;
+  Category _selectedCategor = Category.leisure;
+
+  void _presentDAtePicker() async {
+    final now = DateTime.now();
+    final pickedDate = await showDatePicker(
+        context: context,
+        firstDate: DateTime(now.year - 1, now.month, now.day),
+        lastDate: now,
+        initialDate: now);
+    setState(() {
+      _selecterDate = pickedDate;
+    });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16),
+    return Padding(
+      padding: const EdgeInsets.all(16),
       child: Column(
         children: [
           TextField(
+            controller: _titleController,
             maxLength: 50,
             keyboardType: TextInputType.name,
-            decoration: InputDecoration(label: Text('Title')),
+            decoration: const InputDecoration(label: Text('Title')),
           ),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _amountController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      label: Text("Amount"), prefix: Text('\$')),
+                ),
+              ),
+              const SizedBox(
+                width: 16,
+              ),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(_selecterDate == null
+                        ? 'No date selected'
+                        : formatter.format(_selecterDate!)),
+                    IconButton(
+                        onPressed: _presentDAtePicker,
+                        icon: Icon(Icons.calendar_month))
+                  ],
+                ),
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            children: [
+              DropdownButton(
+                  value: _selectedCategor,
+                  items: Category.values
+                      .map(
+                        (data) => DropdownMenuItem(
+                          value: data,
+                          child: Text(
+                            data.name.toUpperCase(),
+                          ),
+                        ),
+                      )
+                      .toList(),
+                  onChanged: (value) {
+                    if (value == null) {
+                      return;
+                    }
+                    setState(() {
+                      _selectedCategor = value;
+                    });
+                  }),
+              const Spacer(),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text('Cancel')),
+              ElevatedButton(
+                  onPressed: () {
+                    print(_titleController.text);
+                    print(_amountController.text);
+                  },
+                  child: const Text('Save Expense'))
+            ],
+          )
         ],
       ),
     );
